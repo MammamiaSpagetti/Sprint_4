d55774d1f9288d36fc1eb86cb9ff2ca0673b7fde
MammamiaSpagetti <85455158+MammamiaSpagetti@users.noreply.github.com>
2026-06-15 08:34:59 +0300

    delete fixtures from tests.py

--- tests.py
+++ tests.py
@@ -1,29 +1,6 @@
 import pytest
 from main import BooksCollector
 
-@pytest.fixture
-def collector():
-    return BooksCollector()
-
-@pytest.fixture
-def collector_with_books(collector):
-    for book in ['Дюна', 'Оно', 'Том и Джерри']:
-        collector.add_new_book(book)
-    return collector
-
-@pytest.fixture
-def collector_with_genres(collector_with_books):
-    collector_with_books.set_book_genre('Дюна', 'Фантастика')
-    collector_with_books.set_book_genre('Оно', 'Ужасы')
-    collector_with_books.set_book_genre('Том и Джерри', 'Мультфильмы')
-    return collector_with_books
-
-@pytest.fixture
-def collector_with_favorites(collector_with_books):
-    collector_with_books.add_book_in_favorites('Дюна')
-    collector_with_books.add_book_in_favorites('Оно')
-    return collector_with_books
-
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
